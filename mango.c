f17fa7b845b7d16e16f271a264ba6c6c125e533f
Marc Cavigli <mcavigli@gmail.com>
2019-04-16 23:47:59 +0000

    Added new function that splits child from main

--- mango.c
+++ mango.c
@@ -1,17 +1,17 @@
 #include "header.h"
 /**
  * main - a simple shell
- *
+ * @argc: argument counter ((unused))
+ * @argv: argument vector
  * Return: 0
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	int status, line;
-        int counter = 0, builtfunc_ret = 0;
-        char *buff = NULL, *cmd = NULL;
-        char **bigb = NULL;
-        size_t size = 0;
-        pid_t child_pd;
+	int line;
+	int counter = 0, builtfunc_ret = 0;
+	char *buff = NULL, *cmd = NULL;
+	char **bigb = NULL;
+	size_t size = 0;
 
 	signal(SIGINT, SIG_IGN);
 	while (1)
@@ -33,22 +33,38 @@ int main(int argc __attribute__((unused)), char **argv)
 		if (builtfunc_ret == -1)
 			break;
 		cmd = get_env(bigb[0]);
-		child_pd = fork();
-		if (child_pd == -1)
-			return (child_fail(buff, bigb));
-		if (child_pd == 0)
-		{
-			if (cmd == NULL)
-				cmd = "";
-			if (execve(cmd, bigb, NULL) == -1)
-				return(execerror(buff, bigb, counter, argv[0]));
-		}
-		else
-		{
-			wait(&status);
-			free_parent(buff, bigb, cmd);
-		}
+		child_split(buff, bigb, cmd, argv, counter);
 	}
 	free_some(buff, bigb);
 	return (0);
+}
+
+/**
+ * child_split - runs the child process
+ * @buff: user input
+ * @bigb: array of strings that hold user input as well any flags
+ * @cmd: concatenated string of user input
+ * @argv: argument vector, used to hold the executable of this shell
+ * @counter: a count of how many times the loop runs
+ */
+void child_split(char *buff, char **bigb, char *cmd, char **argv, int counter)
+{
+	pid_t child_pd;
+	int status;
+
+	child_pd = fork();
+	if (child_pd == -1)
+		child_fail(buff, bigb);
+	if (child_pd == 0)
+	{
+		if (cmd == NULL)
+			cmd = "";
+		if (execve(cmd, bigb, NULL) == -1)
+			execerror(buff, bigb, counter, argv[0]);
+	}
+	else
+	{
+		wait(&status);
+		free_parent(buff, bigb, cmd);
+	}
 }
